0063b6a9abe7237d5c870f5b19a94c4c5e18f0ea
aayush-mhrzn7 <aayushmaharjan497@gmail.com>
2024-03-16 12:09:23 +0545

    world peas farmer website

---
+++ world pea/main.js
@@ -0,0 +1,47 @@
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null;
+
+document.addEventListener("DOMContentLoaded", initializeSlider);
+
+function initializeSlider(){
+    if(slides.length > 0){
+        slides[slideIndex].classList.add("displaySlide");
+        intervalId = setInterval(nextSlide, 3000);
+    }
+}
+
+function showSlide(index){
+    if(index >= slides.length){
+        slideIndex = 0;
+    }
+    else if(index < 0){
+        slideIndex = slides.length - 1;
+    }
+
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide");
+    });
+    slides[slideIndex].classList.add("displaySlide");
+}
+
+function prevSlide(){
+    clearInterval(intervalId);
+    slideIndex--;
+    showSlide(slideIndex);
+}
+
+function nextSlide(){
+    slideIndex++;
+    showSlide(slideIndex);
+}
+
+/* responsive */
+const toggle = document.getElementById('header-toggle');
+const nav = document.getElementById('nav-m');
+const named = document.getElementById('name');
+toggle.addEventListener('click',()=>{
+nav.classList.toggle('show');
+toggle.classList.toggle('bx-x');
+named.classList.toggle('block');
+})
